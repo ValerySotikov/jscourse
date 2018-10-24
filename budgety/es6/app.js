29a218585bf1d883133d69b1877f3bc23452a81e
imperivm_trade_02 <valery.sotikov@imperivm.team>
2018-10-24 17:57:17 +0500

    refactor budgety with es6 standard. At least I tried

--- budgety/es6/app.js
+++ budgety/es6/app.js
@@ -3,8 +3,18 @@ import UIController from './ui';
 
 class AppController {
   constructor(budgetCtrl, uiCtrl) {
-
+    this.budgetCtrl = budgetCtrl;
+    this.uiCtrl = uiCtrl;
   };
+
+  setupEventListener() {
+    const DOM = this.uiCtrl.
+  }
 };
+
+AppController(
+  new BudgetController(),
+  new UIController()
+).init();
 
 
